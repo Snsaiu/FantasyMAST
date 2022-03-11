77775930e087a7317d4bd4259aaf57c5e0528f65
Saiu <34592712+Snsaiu@users.noreply.github.com>
2022-03-12 00:11:21 +0800

    添加服务发现

---
+++ FantasyMASTApplication/DiscoverDevicesApplication.cs
@@ -0,0 +1,31 @@
+﻿namespace FantasyMASTApplication;
+
+using FantasyResultModel;
+
+using LocalNetTransformImpl;
+
+using TransformInterface;
+using TransformInterface.Models;
+
+/// <summary>
+/// 设备发现服务
+/// </summary>
+public class DiscoverDevicesApplication
+{
+    private IDiscoverDevices udpDiscoverDevices = null;
+    public DiscoverDevicesApplication()
+    {
+        this.udpDiscoverDevices = new UdpDiscoverDeviceImpl();
+    }
+
+    /// <summary>
+    /// 设备发现
+    /// </summary>
+    /// <param name="token">设备发送的密文</param>
+    /// <returns>返回结果</returns>
+    public Task<ResultBase< List<DiscoveredDeviceModel>>> DiscoverAsync(string token)
+    {
+
+
+    }
+}
---
+++ TerminalClient/CommandParse/Impls/DiscoverDeviceCommandParse.cs
@@ -0,0 +1,52 @@
+﻿namespace TerminalClient.CommandParse.Impls;
+
+using FantasyResultModel;
+using FantasyResultModel.Impls;
+
+/// <summary>
+/// 设备发现
+/// </summary>
+public class DiscoverDeviceCommandParse : CommandParseBase
+{
+    public DiscoverDeviceCommandParse(string command)
+        : base(command)
+    {
+    }
+
+    protected override ResultBase<bool> ParseCommand(List<string> flags)
+    {
+
+        if (flags[0]=="discover")
+        {
+            if (flags.Count==2)
+            {
+                //获得当前用户已知的所有蓝牙设备
+                switch (flags[1])
+                {
+                    case "-b-all-list":
+                        break;
+                    //获得当前用户已知的并在线的蓝牙设备
+                    case "-b-online":
+                        break;
+                    //获得当前用户已知的所有局域网设备
+                    case "-n-all-list":
+                        break;
+                    case "-n-online-list":
+                        break;
+                }
+
+            }
+            else
+            {
+                ConsoleHelper.WriteCommandParamCountErrorLine("discover");
+                return new SuccessResultModel<bool>(false);
+            }
+        }
+        else
+        {
+            return new SuccessResultModel<bool>(false);
+        }
+
+
+    }
+}
